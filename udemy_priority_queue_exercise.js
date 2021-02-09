7d5191f32eff95bac91bea30deb2c7c124483eee
Crew Spence <csspence123@gmail.com>
2021-02-09 07:16:37 -0600

    Wrote dequeue and bubbleDown methods.

--- udemy_priority_queue_exercise.js
+++ udemy_priority_queue_exercise.js
@@ -49,53 +49,54 @@ class PriorityQueue {
     }
   }
 
-  // extractMax () {
-  //   let finalIdx = this.values.length - 1;
-  //   let firstIdx = 0;
-  //   let placeHolder = this.values[finalIdx];
-  //   this.values[finalIdx] = this.values[firstIdx];
-  //   this.values[firstIdx] = placeHolder;
-  //   placeHolder = this.values[finalIdx];
-  //   this.values.pop();
-  //   this.bubbleDown();
-  //   return placeHolder;
-  // }
+  dequeue () {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if(this.values.length > 0) {
+      this.values[0] = end;
+      this.bubbleDown();
+    }
+    return min;
+  }
 
-  // bubbleDown () {
-  //   let idx = 0;
-  //   const length = this.values.length;
-  //   let element = this.values[0];
-  //   while(true) {
-  //     let leftChildIdx = 2 * idx + 1;
-  //     let rightChildIdx = 2 * idx + 2;
-  //     let leftChild, rightChild;
-  //     let swap = null;
+  bubbleDown () {
+    let idx = 0;
+    const length = this.values.length;
+    let element = this.values[0];
+    while(true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
 
-  //     if(leftChildIdx < length) {
-  //       leftChild = this.values[leftChildIdx];
-  //       if(leftChild > element) {
-  //         swap = leftChildIdx;
-  //       }
-  //     }
-  //     if(rightChildIdx < length) {
-  //       rightChild = this.values[rightChildIdx];
-  //       if(
-  //         (swap === null && rightChild > element) || 
-  //         (swap !== null && rightChild > leftChild)
-  //       ) {
-  //         swap = rightChildIdx;
-  //       }
-  //     }
-  //     if(swap === null) break;
-  //     this.values[idx] = this.values[swap];
-  //     this.values[swap] = element;
-  //     idx = swap
-  //   }
-  // }
+      if(leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if(leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if(rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if(
+          (swap === null && rightChild.priority < element.priority) || 
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if(swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap
+    }
+  }
   
 }
 
-let test = new PriorityQueue;
-test.enqueue('noder', 10);
-test.enqueue('noder', 4);
-console.log(test);
+// let test = new PriorityQueue;
+// test.enqueue('noder', 10);
+// test.enqueue('noderer', 4);
+// test.enqueue('a thing', 27);
+// test.enqueue('oooh', 2);
+// test.dequeue();
+// console.log(test);
